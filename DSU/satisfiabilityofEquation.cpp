c77ad41138786f74cd24f65a33dd053a9b377870
001sarvesh <97980500+001sarvesh@users.noreply.github.com>
2024-09-21 19:26:33 +0530

    Update satisfiabilityofEquation.cpp

--- DSU/satisfiabilityofEquation.cpp
+++ DSU/satisfiabilityofEquation.cpp
@@ -1,53 +1,53 @@
 class Solution {
 public:
-    vector<int> parent;
-    vector<int> rank;
+    vector<int>parent;
+    vector<int>rank;
 
-    int find(int x) {
-        if (parent[x] == x) {
+    int find(int x){
+        if(parent[x] == x){
             return x;
         }
         return parent[x] = find(parent[x]);
     }
 
-    void Union(int x, int y) {
+    void Union(int x, int y){
         int x_parent = find(x);
         int y_parent = find(y);
 
-        if (rank[x_parent] > rank[y_parent]) {
-            parent[x_parent] = y_parent;
-        } else if (rank[x_parent] < rank[y_parent]) {
+        if(rank[x_parent] > rank[y_parent]){
             parent[y_parent] = x_parent;
-        } else {
+        }
+        else if(rank[y_parent] > rank[x_parent]){
+            parent[x_parent] = y_parent;
+        }
+        else{
             parent[x_parent] = y_parent;
             rank[y_parent]++;
         }
     }
-
     bool equationsPossible(vector<string>& equations) {
         parent.resize(26);
-        rank.resize(26, 0);
+        rank.resize(26,0);
 
-        for(int i = 0; i < 26; i++) {
+        for(int i=0; i<26; i++){
             parent[i] = i;
         }
-        // Union of equal chars
-        for(string &s : equations) {
-            if (s[1] == '=') { // "a==b"
-                Union(s[0] - 'a', s[3] - 'a');
+        //union for equal chars
+        for(string &s: equations){
+            if(s[1] == '='){ // a == b
+                Union(s[0]-'a',s[3]-'a');
             }
         }
-
-        // Now to handle != case
-        for(string &s : equations) {
-            if (s[1] == '!') {
+        // now to handle != case
+        for(string &s: equations){
+            if(s[1] == '!'){
                 char first = s[0];
                 char second = s[3];
 
                 int parent_first = find(first - 'a');
                 int parent_second = find(second - 'a');
 
-                if (parent_first == parent_second) {
+                if(parent_first == parent_second){
                     return false;
                 }
             }
